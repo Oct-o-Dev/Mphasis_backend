675b91a0a365cf053c71cd05c2975402aa02dda8
Divyam Tiwari <divyam2701@gmail.com>
2026-06-10 11:40:38 +0530

    Added RaiseTicketServiceDTO

---
+++ csp/src/main/java/com/mphasis/csp/dto/RaiseTicketServiceDTO.java
@@ -0,0 +1,20 @@
+package com.mphasis.csp.dto;
+
+import com.mphasis.csp.enums.ServiceType;
+import com.mphasis.csp.enums.TicketStatus;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class RaiseTicketServiceDTO {
+
+    @NotNull
+    private Integer ticketId;
+
+    @NotNull
+    private ServiceType serviceType;
+
+    private String comment;
+
+    private TicketStatus newStatus;
+}

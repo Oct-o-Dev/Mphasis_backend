d986f4364b81a2656092308c3d3897df9207d8f9
Divyam Tiwari <divyam2701@gmail.com>
2026-06-12 15:16:09 +0530

    added getStatusUpdate function

--- csp/src/main/java/com/mphasis/csp/enums/ServiceAction.java
+++ csp/src/main/java/com/mphasis/csp/enums/ServiceAction.java
@@ -1,8 +1,18 @@
 package com.mphasis.csp.enums;
 
 public enum ServiceAction {
+
+    // Does not update status
     COMMENT,
-    RETURN,
-    CLOSE,
-    ESCALATE
+
+    // Step down
+    RETURN_TO_CUSTOMER,
+
+    // Step up
+    ESCALATE_TO_CRO,
+    ESCALATE_TO_MANAGER,
+
+    // Close
+    RESOLVE,
+    REJECT
 }
--- csp/src/main/java/com/mphasis/csp/enums/TicketStatus.java
+++ csp/src/main/java/com/mphasis/csp/enums/TicketStatus.java
@@ -5,5 +5,54 @@ public enum TicketStatus {
     PENDING_CRO,
     PENDING_MANAGER,
     CLOSED_RESOLVED,
-    CLOSED_REJECTED
+    CLOSED_REJECTED;
+
+    public TicketStatus getStatusUpdate(ServiceAction action) {
+
+        // On COMMENT : no state change
+        if (action == ServiceAction.COMMENT) {
+            return this;
+        }
+
+        return switch (this) {
+
+            case PENDING_CUSTOMER -> switch (action) {
+                case ESCALATE_TO_CRO -> TicketStatus.PENDING_CRO;
+                default -> {
+                    printInvalidStatusUpdate(action);
+                    yield this;
+                }
+            };
+
+            case PENDING_CRO -> switch (action) {
+                case RETURN_TO_CUSTOMER -> TicketStatus.PENDING_CUSTOMER;
+                case ESCALATE_TO_MANAGER -> TicketStatus.PENDING_MANAGER;
+                case RESOLVE -> TicketStatus.CLOSED_RESOLVED;
+                case REJECT -> TicketStatus.CLOSED_REJECTED;
+                default -> {
+                    printInvalidStatusUpdate(action);
+                    yield this;
+                }
+            };
+
+            case PENDING_MANAGER -> switch (action) {
+                case RETURN_TO_CUSTOMER -> TicketStatus.PENDING_CUSTOMER;
+                case RESOLVE -> TicketStatus.CLOSED_RESOLVED;
+                case REJECT -> TicketStatus.CLOSED_REJECTED;
+                default -> {
+                    printInvalidStatusUpdate(action);
+                    yield this;
+                }
+            };
+
+            case CLOSED_REJECTED, CLOSED_RESOLVED -> {
+                printInvalidStatusUpdate(action);
+                yield this;
+            }
+        };
+    }
+
+    private void printInvalidStatusUpdate(ServiceAction action) {
+        System.out.println("Invalid State Update\n      STATUS: " + this.name() + " ACTION: " + action.name());
+    }
 }
